104307c43c4019eb00f38195daa8091ef454ce3f
Kayky de Brito <kaykyb@users.noreply.github.com>
2019-03-09 22:22:16 -0300

    Improves constants usability

--- src/env.ts
+++ src/env.ts
@@ -1,1 +1,11 @@
-export const IS_DEV = true;
+export const ENV = {
+  IS_DEV: true,
+
+  INTERNAL_SETTINGS_APPDATA_PATH: "preferences.json",
+  USER_SETTINGS_APPDATA_PATH: "User/settings.json",
+
+  INSTALLED_THEMES_APPDATA_PATH: "User/installed-themes",
+
+  INSTALLED_EXT_PROTOCOL: "telurio-ext://",
+  INTERNAL_EXT_PROTOCOL: "telurio-internal-ext://"
+};
