669d7a6630a9d2c472cbbd9fd086f8ca4cad5441
Kayky de Brito <kaykyb@users.noreply.github.com>
2019-03-13 12:47:38 -0300

    Remove unused code

--- src/common/browser/browseri18nService.ts
+++
@@ -1,5 +0,0 @@
-import I18nLanguageFile from "../node/services/i18n/i18nLanguageFile";
-
-export default class BrowserI18nService {
-  constructor(public readonly languageFile: I18nLanguageFile) {}
-}
