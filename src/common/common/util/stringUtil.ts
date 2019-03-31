b619f5e85661bb2c9754acb0ff07e8f5be41bb6c
Kayky de Brito <kaykyb@users.noreply.github.com>
2019-03-31 00:42:11 -0300

    Add method to help with loc to string util

--- src/common/common/util/stringUtil.ts
+++ src/common/common/util/stringUtil.ts
@@ -1,4 +1,14 @@
+import Label from "../extensions/manifest-type/label";
+
 export default class StringUtil {
+  public static getAppropriateLabel(from: Label[], lang: string): Label {
+    const locLabel = from.find(l => l.lang === lang);
+    const enLabel = from.find(l => l.lang === "en");
+    const zeroLabel = from[0];
+
+    return locLabel || enLabel || zeroLabel;
+  }
+
   // based on stackoverflow answer --> https://stackoverflow.com/a/4673436 CC BY-SA
   public static format(str: string, ...args: string[]): string {
     return str.replace(/{(\d+)}/g, (match, n) => {
