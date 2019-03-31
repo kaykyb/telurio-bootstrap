87cd7c35bdd4fa1553f817e6c4c1430798a97689
Kayky de Brito <kaykyb@users.noreply.github.com>
2019-03-31 00:41:07 -0300

    Add new contributions

--- src/common/common/extensions/manifest-type/extensionContributions.ts
+++ src/common/common/extensions/manifest-type/extensionContributions.ts
@@ -1,11 +1,13 @@
 import ExtensionPermission from "./extensionPermission";
 import ExtensionCommand from "./extensionCommand";
 import SettingMetadata from "@src/common/node/services/settings/settingMetadata";
+import ExtensionSettingsSection from "./extensionSettingsSection";
 
 export default class ExtensionContributions {
   constructor(
     public commands?: ExtensionCommand[],
     public permissions?: ExtensionPermission[],
-    public settings?: Array<SettingMetadata<any>>
+    public settings?: Array<SettingMetadata<any>>,
+    public settingsSections?: ExtensionSettingsSection[]
   ) {}
 }
